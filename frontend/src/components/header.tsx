72f807cb235bf880ae56c5a740a68118e846a03e
skolov1 <voloks1@outlook.com>
2026-09-22 17:31:13 -0300

    chore: criação e remoção de pastas do frontend

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,12 @@
+// interface HeaderProps {
+//   propName: type;
+// }
+
+export default function Header() {
+  return (
+    <header className="h-24 w-full bg-green-800 flex items-center justify-center">
+      <h1 className="text-3xl text-white">ONG do Povo - Praia Grande</h1>
+
+    </header>
+  );
+}
